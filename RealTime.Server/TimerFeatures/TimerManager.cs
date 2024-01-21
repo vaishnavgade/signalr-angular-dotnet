bc2b74b24580c0f07c5ce9734a744d2084151307
vaishnavgade <vaishnav.gade@gmail.com>
2024-01-21 15:00:42 -0500

    Implement a timer manager

---
+++ RealTime.Server/TimerFeatures/TimerManager.cs
@@ -0,0 +1,33 @@
+namespace Realtime.Server.TimerFeatures;
+
+public class TimerManager
+{
+    private Timer? _timer;
+    private AutoResetEvent? _autoResetEvent;
+    private Action? _action;
+
+    public DateTime TimerStarted { get; set; }
+    public bool IsTimerStarted { get; set; }
+
+    public void PrepareTimer(Action action)
+    {
+        _action = action;
+        _autoResetEvent = new AutoResetEvent(false);
+        
+        // The timer will make a one-second pause before the first execution.
+        _timer = new Timer(Excute, _autoResetEvent, 1000, 2000);
+        TimerStarted = DateTime.Now;
+        IsTimerStarted = true;
+    }
+
+    public void Excute(object? stateInfo)
+    {
+        _action();
+
+        if((DateTime.Now - TimerStarted).TotalSeconds > 60)
+        {
+            IsTimerStarted = false;
+            _timer?.Dispose();
+        }
+    }
+}
